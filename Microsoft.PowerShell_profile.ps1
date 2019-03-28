ba3174af4a3692681ce7cb0c0975d62626130c56
teknologyguru <37561904+teknologyguru@users.noreply.github.com>
2019-03-27 22:07:39 -0500

    Add oh-my-posh

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,6 +9,10 @@ function prompt {
     Write-Output "${msg}$( Get-Date -UFormat "%I:%M%p" ) $(Get-Location)> "
 }
 
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme -name Agnoster
+
 if ($host.UI.RawUI.WindowTitle -match "Administrator") {
     $host.UI.RawUI.BackgroundColor = "DarkRed";
     $host.UI.RawUI.ForegroundColor = "White";
